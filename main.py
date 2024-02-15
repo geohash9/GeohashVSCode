22b9346c9546f215ea936ac5e98c05e02b331169
geohash9 <geohash9@gmail.com>
2024-02-15 12:37:18 +0530

    Your commit message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import psycopg2
-
+import os
 app = Flask(__name__)
 
 # Database connection parameters
@@ -36,5 +36,5 @@ def get_location():
     else:
         return jsonify({"error": "Custom geohash code not found"})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
